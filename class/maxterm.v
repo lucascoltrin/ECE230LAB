d6ded2391b81dfaa34dddb7385b98c3237acdbde
lucascoltrin <lucas.coltrin@gmail.com>
2025-09-16 09:44:05 -0600

    Week 4 Lab 4 - SOP POS and KMaps

---
+++ class/maxterm.v
@@ -0,0 +1,10 @@
+module maxterm (
+    input A, B, C, D,
+    output Y
+);
+
+assign Y = (B | C | D) &
+           (~B | ~D) &
+           (~A | ~D);// Enter your equation here
+
+endmodule
---
+++ class/minterm.v
@@ -0,0 +1,10 @@
+module minterm (
+    input A, B, C, D,
+    output Y
+);
+
+assign Y = (B & ~C & ~D) |
+           (~A & ~B & D) |
+           (C & ~D); // Enter your equation here
+
+endmodule
